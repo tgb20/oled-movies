27d3c06a64685b29e2b3a77f9f66e3b80eaa14c9
jacob <j@cob.email>
2024-09-20 10:58:44 -0400

    Update to vite app

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import Carousel from "primevue/carousel";
+import { ref } from "vue";
+
+interface Movie {
+  genres: string;
+  id: string;
+  poster: string;
+  rated: string;
+  rating: number;
+  title: string;
+  votes: string;
+  watched: string;
+  year: number;
+}
+
+const movies = ref<Movie[]>([]);
+
+fetch("https://flowers.tgb.gg/movies")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data: Movie[]) => {
+    console.log(data);
+    movies.value = data
+      .sort((a, b) => {
+        return Date.parse(b.watched) - Date.parse(a.watched);
+      })
+      .slice(0, 10);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+</script>
+
+<template>
+  <div class="flex items-center h-screen">
+    <Carousel
+      :value="movies"
+      :numVisible="3"
+      :numScroll="1"
+      circular
+      :autoplayInterval="3000"
+    >
+      <template #item="slotProps">
+        <div class="rounded m-2">
+          <div class="mb-4">
+            <div class="relative mx-auto">
+              <img
+                :src="slotProps.data.poster"
+                :alt="slotProps.data.title"
+                class="w-full rounded"
+              />
+            </div>
+          </div>
+        </div>
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<style scoped></style>
